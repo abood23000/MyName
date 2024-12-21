0101d322c8ccbe95db5756d005d47dfccd32c6d6
abood23000 <an3rb99@gmail.com>
2024-12-22 00:55:26 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/name', (req, res) => {
+    res.send('Abd Al-Rahman Froukh'); 
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
